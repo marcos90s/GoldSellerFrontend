111a82e19bf08f8320ec45e919b1e75f49dc3a0b
Marcos Antônio <marcosabes@hotmail.com>
2025-06-25 21:58:45 -0300

    /home router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,6 @@ export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'home', component: HomeComponent},
     {path: 'register', component: RegisterComponent},
-    {path: 'users', component: UserListComponent}
-    //{path: '', redirectTo: '/home', pathMatch: 'full'}
+    {path: 'users', component: UserListComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
