14acb4f68a38c473d968c190365f162b2876b75d
Marcos Antônio <marcosabes@hotmail.com>
2025-07-07 22:33:19 -0300

    Add router for logged user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,13 +9,20 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
-  constructor(private router: Router){}
+export class HomeComponent implements OnInit{
+  
+  constructor(private router: Router, private authService: AuthService){}
+  isLoggedIn: boolean = this.authService.isLoggedIn()
+  ngOnInit(): void {
+    if(this.authService.isLoggedIn()){
+      this.router.navigate(['/game'])
+    }
+  }
   
   homeText: string = 'O Gold Seller foi criado para te ajudar a gerenciar sua venda de gold! Se você ainda não tem uma conta, registre-se'
 
   registrar(){
     this.router.navigate(['/register'])
   }
+ 
 }
