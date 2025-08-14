1ce3f61d197b6d57a3e0d1df037d7b0c42c81d16
Marcos Vieira <marcosvieira90ss@gmail.com>
2025-08-14 14:30:44 -0300

    Removed: router to /game endpoint on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,9 +14,7 @@ export class HomeComponent implements OnInit{
   constructor(private router: Router, private authService: AuthService){}
   isLoggedIn: boolean = this.authService.isLoggedIn()
   ngOnInit(): void {
-    if(this.authService.isLoggedIn()){
-      this.router.navigate(['/game'])
-    }
+    
   }
   
   homeText: string = 'O Gold Seller foi criado para te ajudar a gerenciar sua venda de gold! Se você ainda não tem uma conta, registre-se'
